a6617b9bd6634bd5700db7ad5f7419ede83c299e
Timur Gafarov <gecko0307@gmail.com>
2016-11-03 18:07:31 +0300

    More geometries and Character support

--- README.md
+++ README.md
@@ -2,4 +2,11 @@ gmdmech
 =======
 This is a work-in-progress [dmech](https://github.com/gecko0307/dmech) physics engine wrapper for Game Maker 8, designed specifically to work with [Xtreme3D 3.x](https://github.com/xtreme3d/xtreme3d). Xtreme3D already includes a physics engine, ODE, but it has some pitfalls and limitations (and actually is not up to date with latest ODE), so it would be nice to have an alternative. dmech is, in turn, a simple, but fully-functional game-oriented physics library, which can be used both for dynamics and kinematics simultaneously.
 
-Currently gmdmech wraps only basic functions of dmech, such as creation of physics worlds, adding static and dynamic bodies and attaching geometric shapes. It also includes a number of helper functions for integration with Xtreme3D (syncing bodies with objects, etc). Look dmech-demo.gmk for details.
+Currently gmdmech provides the following functionality:
+* Creation of physics worlds
+* Static and dynamic bodies
+* Geometric shapes for bodies: sphere, box, cylinder, cone, ellipsoid
+* Built-in character controller that can move, jump and interact with other bodies - very useful for games
+* A number of helper functions for integration with Xtreme3D (syncing bodies with objects, etc).
+
+There is a simple Xtreme3D-based example (dmech-demo.gmk) that demonstrates how to create bodies and a character that is controlled by the player.
--- main.d
+++ main.d
@@ -19,6 +19,7 @@ import dmech.geometry;
 import dmech.shape;
 import dmech.constraint;
 import dmech.raycast;
+import character;
 
 __gshared int numPhysicsWorlds = 0;
 
@@ -112,6 +113,12 @@ export double dmWorldAddCollisionShape(double w, double b, double g, double px, 
         return 0.0;
 }
 
+export double dmCreateGeomSphere(double r)
+{
+    GeomSphere geom = New!(GeomSphere)(r);
+    return geom.gmptr;
+}
+
 export double dmCreateGeomBox(double hsx, double hsy, double hsz)
 {
     GeomBox geom = New!(GeomBox)(Vector3f(hsx, hsy, hsz));
@@ -122,6 +129,28 @@ export double dmCreateGeomCylinder(double h, double r)
 {
     GeomCylinder geom = New!(GeomCylinder)(h, r);
     return geom.gmptr;
+}
+
+export double dmCreateGeomCone(double h, double r)
+{
+    GeomCone geom = New!(GeomCone)(h, r);
+    return geom.gmptr;
+}
+
+export double dmCreateGeomEllipsoid(double rx, double ry, double rz)
+{
+    GeomEllipsoid geom = New!(GeomEllipsoid)(Vector3f(rx, ry, rz));
+    return geom.gmptr;
+}
+
+export double dmDeleteGeom(double g)
+{
+    Geometry geom = g.toObject!(Geometry);
+    if (geom)
+    {
+        Delete(geom);
+    }
+    return 1.0;
 }
 
 export double dmBodyGetPosition(double b, double index)
@@ -156,6 +185,123 @@ export double dmBodyGetUp(double b, double index)
         Matrix4x4f m = rb.transformation;
         Vector3f u = m.up;
         return u.arrayof[cast(uint)index];
+    }
+    else
+        return 0.0;
+}
+
+export double dmCharacterCreate(double w, double g, double px, double py, double pz, double mass)
+{
+    PhysicsWorld world = w.toObject!(PhysicsWorld);
+    Geometry geom = g.toObject!(Geometry);
+    if (world && geom)
+    {
+        CharacterController cc = New!(CharacterController)(world, Vector3f(px, py, pz), mass, geom);
+        return cc.gmptr;
+    }
+    else
+        return 0.0;
+}
+
+export double dmCharacterCreateSensorShape(double c, double g, double px, double py, double pz)
+{
+    CharacterController cc = c.toObject!(CharacterController); 
+    Geometry geom = g.toObject!(Geometry);
+    if (cc && geom)
+    {     
+        ShapeComponent s = cc.createSensor(geom, Vector3f(px, py, pz));
+        return s.gmptr;
+    }
+    else
+        return 0.0;
+}
+
+export double dmCharacterGetBody(double c)
+{
+    CharacterController cc = c.toObject!(CharacterController); 
+    if (cc)
+    {     
+        return cc.rbody.gmptr;
+    }
+    else
+        return 0.0;
+}
+
+export double dmCharacterMove(double c, double dx, double dy, double dz, double speed)
+{
+    CharacterController cc = c.toObject!(CharacterController); 
+    if (cc)
+    {     
+        cc.move(Vector3f(dx, dy, dz), speed);
+        return 1.0;
+    }
+    else
+        return 0.0;
+}
+
+export double dmCharacterJump(double c, double height)
+{
+    CharacterController cc = c.toObject!(CharacterController); 
+    if (cc)
+    {     
+        cc.jump(height);
+        return 1.0;
+    }
+    else
+        return 0.0;
+}
+
+export double dmCharacterUpdate(double c, double clampy)
+{
+    CharacterController cc = c.toObject!(CharacterController); 
+    if (cc)
+    {     
+        cc.update(cast(bool)cast(uint)clampy);
+        return 1.0;
+    }
+    else
+        return 0.0;
+}
+
+export double dmCharacterIsOnGround(double c)
+{
+    CharacterController cc = c.toObject!(CharacterController); 
+    if (cc)
+    {     
+        return cc.onGround;
+    }
+    else
+        return 0.0;
+}
+
+export double dmCharacterGetGroundPosition(double c, double index)
+{
+    CharacterController cc = c.toObject!(CharacterController); 
+    if (cc)
+    {     
+        return cc.floorPosition.arrayof[cast(uint)index];
+    }
+    else
+        return 0.0;
+}
+
+export double dmCharacterGetGroundNormal(double c, double index)
+{
+    CharacterController cc = c.toObject!(CharacterController); 
+    if (cc)
+    {     
+        return cc.floorNormal.arrayof[cast(uint)index];
+    }
+    else
+        return 0.0;
+}
+
+export double dmCharacterGetGroundBody(double c)
+{
+    CharacterController cc = c.toObject!(CharacterController); 
+    if (cc)
+    {     
+        return cc.floorBody.gmptr;
     }
     else
         return 0.0;
